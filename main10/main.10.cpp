6e381b9bcaa381312d9dae474ba28d5ba8c196a4
SouthCastle <67773599+tjdgus6255@users.noreply.github.com>
2022-01-01 14:33:29 +0900

    inheritance

---
+++ main10/main.10.cpp
@@ -0,0 +1,30 @@
+#include "Student.h"
+#include "Teacher.h"
+
+using namespace std;
+
+int main()
+{
+	Student std("Jack Jack");
+	std.setName("Jack Jack 2");
+	std::cout << std.getName() << std::endl;;
+
+	Teacher teacher1("Dr. H");
+	teacher1.setName("Dr. K");
+
+	std::cout << teacher1.getName() << endl;
+
+	std::cout << std << std::endl;
+	std::cout << teacher1 << std::endl;
+
+	std.doNothing();
+	teacher1.doNothing();
+
+	std.study();
+	teacher1.teach();
+
+	Person person;
+	person.setName("Mr.Incredible");
+	person.getName();
+	return 0;
+}
